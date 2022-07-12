328a3bdc27204f0e26521d1f715be60fcd460c72
Huzhva-Yaroslav <91682192+Huzhva-Yaroslav@users.noreply.github.com>
2022-07-12 20:59:34 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,51 +1,56 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close')
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+  menu.classList.add('active')
+})
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
+  menu.classList.remove('active')
+})
 
-// const counters = document.querySelectorAll('.skills__ratings-counter'),
-//       lines = document.querySelectorAll('.skills__ratings-line span');
+function increase() {
+  // Change the variable to modify the speed of the number increasing from 0 to (ms)
+  let SPEED = 40
+  // Retrieve the percentage value
+  let limit = parseInt(document.getElementById('value1').innerHTML, 10)
+  let limit2 = parseInt(document.getElementById('value2').innerHTML, 10)
+  let limit3 = parseInt(document.getElementById('value3').innerHTML, 10)
+  let limit4 = parseInt(document.getElementById('value4').innerHTML, 10)
+  let limit5 = parseInt(document.getElementById('value5').innerHTML, 10)
+  let limit6 = parseInt(document.getElementById('value6').innerHTML, 10)
 
-// counters.forEach( (item, i) => {
-//     lines[i].style.width = item.innerHTML;
-// });
-// Скролл кнопки
-const scrollBtn = document.getElementById('btn-scroll');
-// console.log(portfolio_scroll);
-scrollBtn.addEventListener('click', handleButtonClick);
-function handleButtonClick() {
-    BlockPortfolio.scrollIntoView({block: "start", behavior: "smooth"});
- }
- const BlockPortfolio = document.getElementById('portfolio_scroll');
-//  Прогресс бар
- window.onscroll = function(e) {
-    e = window.pageYOffset;
-    if (e >= 100) {
-      move();
-    }
+  for (let i = 0; i <= limit; i++) {
+    setTimeout(function () {
+      document.getElementById('value1').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+  for (let i = 0; i <= limit2; i++) {
+    setTimeout(function () {
+      document.getElementById('value2').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+  for (let i = 0; i <= limit3; i++) {
+    setTimeout(function () {
+      document.getElementById('value3').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+  for (let i = 0; i <= limit4; i++) {
+    setTimeout(function () {
+      document.getElementById('value4').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+  for (let i = 0; i <= limit5; i++) {
+    setTimeout(function () {
+      document.getElementById('value5').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+  for (let i = 0; i <= limit6; i++) {
+    setTimeout(function () {
+      document.getElementById('value6').innerHTML = i + '%'
+    }, SPEED * i)
+  }
+}
 
-    function move() {
-        let line = document.getElementById('progres_bar');
-        let width = 0;
-        let interval = setInterval(frame, 20);
-
-      function frame() {
-        if (width >= 90) {
-          clearInterval(interval);
-        } else {
-          width++;
-          line.style.width = width + '%';
-         
-        }
-    }
-
- }
-
-};
+increase()
