8cb80af0643d7650cb1d8fe21e45cfc74600c87b
Huzhva-Yaroslav <91682192+Huzhva-Yaroslav@users.noreply.github.com>
2022-07-14 20:25:57 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,56 +1,75 @@
 const hamburger = document.querySelector('.hamburger'),
-  menu = document.querySelector('.menu'),
-  closeElem = document.querySelector('.menu__close')
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
-  menu.classList.add('active')
-})
+    menu.classList.add('active');
+});
 
 closeElem.addEventListener('click', () => {
-  menu.classList.remove('active')
-})
+    menu.classList.remove('active');
+});
 
 function increase() {
-  // Change the variable to modify the speed of the number increasing from 0 to (ms)
-  let SPEED = 40
-  // Retrieve the percentage value
-  let limit = parseInt(document.getElementById('value1').innerHTML, 10)
-  let limit2 = parseInt(document.getElementById('value2').innerHTML, 10)
-  let limit3 = parseInt(document.getElementById('value3').innerHTML, 10)
-  let limit4 = parseInt(document.getElementById('value4').innerHTML, 10)
-  let limit5 = parseInt(document.getElementById('value5').innerHTML, 10)
-  let limit6 = parseInt(document.getElementById('value6').innerHTML, 10)
+    // Change the variable to modify the speed of the number increasing from 0 to (ms)
+    let SPEED = 40
+    // Retrieve the percentage value
+    let limit = parseInt(document.getElementById('value1').innerHTML, 10)
+    let limit2 = parseInt(document.getElementById('value2').innerHTML, 10)
+    let limit3 = parseInt(document.getElementById('value3').innerHTML, 10)
+    let limit4 = parseInt(document.getElementById('value4').innerHTML, 10)
+    let limit5 = parseInt(document.getElementById('value5').innerHTML, 10)
+    let limit6 = parseInt(document.getElementById('value6').innerHTML, 10)
+  
+    for (let i = 0; i <= limit; i++) {
+      setTimeout(function () {
+        document.getElementById('value1').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+    for (let i = 0; i <= limit2; i++) {
+      setTimeout(function () {
+        document.getElementById('value2').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+    for (let i = 0; i <= limit3; i++) {
+      setTimeout(function () {
+        document.getElementById('value3').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+    for (let i = 0; i <= limit4; i++) {
+      setTimeout(function () {
+        document.getElementById('value4').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+    for (let i = 0; i <= limit5; i++) {
+      setTimeout(function () {
+        document.getElementById('value5').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+    for (let i = 0; i <= limit6; i++) {
+      setTimeout(function () {
+        document.getElementById('value6').innerHTML = i + '%'
+      }, SPEED * i)
+    }
+  }
+  
+  increase()
+  const span = document.querySelectorAll('.linespan');
+  console.log(span);
+  let spanTest = [];
+  for (let index = 0; index < span.length; index++) {
+    const element = span[index];
+    console.log(element);
+    spanTest.push(element);
+  }
 
-  for (let i = 0; i <= limit; i++) {
-    setTimeout(function () {
-      document.getElementById('value1').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-  for (let i = 0; i <= limit2; i++) {
-    setTimeout(function () {
-      document.getElementById('value2').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-  for (let i = 0; i <= limit3; i++) {
-    setTimeout(function () {
-      document.getElementById('value3').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-  for (let i = 0; i <= limit4; i++) {
-    setTimeout(function () {
-      document.getElementById('value4').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-  for (let i = 0; i <= limit5; i++) {
-    setTimeout(function () {
-      document.getElementById('value5').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-  for (let i = 0; i <= limit6; i++) {
-    setTimeout(function () {
-      document.getElementById('value6').innerHTML = i + '%'
-    }, SPEED * i)
-  }
-}
-
-increase()
+  window.addEventListener('scroll', ()=>{
+    let y = window.scrollY;
+    console.log(y);
+    if (y > 2500) {
+      // element.classList.add('active');
+      spanTest.forEach(element => {
+        element.classList.add('active');
+      });
+    }
+  });
